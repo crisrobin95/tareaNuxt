34b55bec0038ee313513e6c66676546e96bc41b7
CristhianQuiñonez <crisrobin-95@hotmail.com>
2025-04-02 09:39:27 +0200

    Tarea 3

---
+++ nuxt-proyect/pages/products/index.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+import { useRouter } from 'nuxt/app';
+import { onMounted } from 'vue';
+
+const router = useRouter();
+
+onMounted(() => {
+  router.push('/');
+});
+</script>
+
+<template>
+  <div>
+    <p>Redirigiendo a la página de inicio...</p>
+  </div>
+</template>
